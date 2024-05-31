d6112dbe43e67378ffd3479b2021b9c05df3ec2e
Gang Plebsu <166305094+GangPlebsu2137@users.noreply.github.com>
2024-05-31 14:41:18 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+
+--// DOCS: https://plebsu-shop.gitbook.io/
+
+description 'Notify FiveM'
+author 'dc: gang_plebsu'
+version '1.0.0'
+
+client_scripts {
+  'client.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+  'html/index.html', 
+  'html/*.js',
+  'html/assets/*.png',
+  'html/style.css'
+}
